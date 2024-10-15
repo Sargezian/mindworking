3ae044e252f0c1aac817b0717785042cfa1396ce
lennartsargezian <Lennart_1997@hotmail.com>
2024-10-15 23:14:02 +0200

    made profil columns responsive

--- src/components/HomeBox/HomeBox.tsx
+++ src/components/HomeBox/HomeBox.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import './HomeBox.css';
-import {ReactTyped} from "react-typed";
+import { ReactTyped } from "react-typed";
 
 const HomeBox: React.FC = React.memo(() => {
     return (
         <div className="image-container">
             <div className="top-container">
-                    <div className="AnimatedTyping-container">
-                        <div className="AnimatedTyping">
-                            <p className="animated-typing">
-                                <ReactTyped
-                                    strings={[
-                                        "My name <br/> is <strong>Lennart Sargezian</strong>",
-                                        "I'm a <br/> <strong>Front-end Developer</strong> ",
-                                        "Let's have a talk over a cup of <br/> <strong>Coffee!!!!</strong>",
-                                    ]}
-                                    typeSpeed={150}
-                                    backSpeed={100}
-                                    loop
-                                />
-                            </p>
-                        </div>
+                <div className="AnimatedTyping-container">
+                    <p className="animated-typing">
+                        <ReactTyped
+                            strings={[
+                                "My name <br/> is <strong>Lennart Sargezian</strong>",
+                                "I'm a <br/> <strong>Front-end Developer</strong> ",
+                                "Let's have a talk over a cup of <br/> <strong>Coffee!!!!</strong>",
+                            ]}
+                            typeSpeed={150}
+                            backSpeed={100}
+                            loop
+                        />
+                    </p>
                 </div>
 
                 <div className="profile-columns">
@@ -31,7 +29,7 @@ const HomeBox: React.FC = React.memo(() => {
                     </div>
                     <div className="column">
                         <div className="text-container">
-                            <p> +45 42 71 99 75 📞</p>
+                            <p>+45 42 71 99 75 📞</p>
                         </div>
                     </div>
                     <div className="column">
